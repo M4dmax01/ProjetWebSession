50328e86eb8e4481915acdfc830fde27b41a0d27
NomaBouh <thbo62126@eleve.isep.fr>
2023-12-06 16:44:01 -0500

    add link switch page sign up to login and revers

---
+++ scripts/signup.js
@@ -0,0 +1,32 @@
+$('#signupForm').on("submit", handleSignUp);
+
+async function handleSignUp(event){
+    event.preventDefault();
+    // Récupérer les valeurs du formulaire
+    var formData = {
+        username: document.getElementById('floatingUsername').value,
+        lastname: document.getElementById('floatingLastname').value,
+        email: document.getElementById('floatingEmail').value,
+        password: document.getElementById('floatingPassword').value,
+        location: document.getElementById('floatingAddress').value,
+        postcode: document.getElementById('floatingPostcode').value,
+        city: document.getElementById('floatingCity').value
+    };
+
+    // Envoyer les données du formulaire au serveur au format JSON
+    fetch('http://localhost:3000/api/user/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData), // Convertir les données du formulaire en chaîne JSON
+    })
+    .then(response => 
+    response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
