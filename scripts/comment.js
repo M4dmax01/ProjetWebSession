53e73c916e1d346cb3f86ea6d03688bf6c0b9c26
REVAUD Clément <revc0901@usherbrooke.ca>
2023-12-07 19:20:53 -0500

    list commentaire profil

--- scripts/comment.js
+++ scripts/comment.js
@@ -102,3 +102,48 @@ async function handleCommentSubmit(event) {
         console.log("Erreur pas d'utilisateur")
     }
 }
+
+//Recupere tout les commentaires de l'api
+async function getAllComments() {
+    try {
+        const response = await fetch(BASE_API + "/comment");
+        const data = await response.json();
+  
+        // Filter out items where clientId is undefined
+        const filteredData = data.filter(item => item.idClient == undefined);
+  
+        populateFoodAnnonces(filteredData);
+    } catch (err) {
+        console.log(err);
+    }
+  }
+
+  //Rempli l'HTML par les foods card
+function populateFoodAnnonces(data) {
+
+    var html = "";
+
+    data.forEach(function(comment) {
+
+        html += `
+            <div class="col-md-6">
+              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                  <strong class="d-inline-block mb-2 text-success">Note : ${comment.mark} / 5</strong>
+                  <h3 class="mb-0">${comment.title}</h3>
+                  <div class="mb-1 text-muted">${moment(comment.date).format('DD/MM/YYYY HH:mm:ss')}</div>
+                  <p class="card-text mb-auto">${comment.description}</p> 
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                </div>
+              </div>
+            </div>
+        `;
+    });
+
+    // change the HTML to the container
+    $('#CommentContainer').html(html);
+}
+
+getAllComments();
